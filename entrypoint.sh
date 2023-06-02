6db9b3a9549548aec1f1b429eb13691c424a3e66
Jim Madge <jmadge@turing.ac.uk>
2023-06-02 13:12:47 +0100

    Improve entrypoint output

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,12 +20,15 @@ until curl -s "$NEXUS_HOST":"$NEXUS_PORT" > /dev/null; do
     echo "Waiting for Nexus"
     sleep 10
 done
+echo "Connected to Nexus"
 
 # Initial configuration
 if [ -f "$NEXUS_DATA_DIR/admin.password" ]; then
     echo "Initial password file present, running initial configuration"
     python3 configure_nexus.py --admin-password "$NEXUS_ADMIN_PASSWORD" --nexus-host "$NEXUS_HOST" --nexus-port "$NEXUS_PORT" change-initial-password --path "$NEXUS_DATA_DIR"
     python3 configure_nexus.py --admin-password "$NEXUS_ADMIN_PASSWORD" --nexus-host "$NEXUS_HOST" --nexus-port "$NEXUS_PORT" initial-configuration --tier "$NEXUS_TIER" --pypi-package-file "$ALLOWLIST_DIR/pypi.allowlist" --cran-package-file "$ALLOWLIST_DIR/cran.allowlist"
+else
+    echo "No initial password file found, skipping initial configuration"
 fi
 
 # Run allowlist configuration AND rerun whenever allowlist files are modified
