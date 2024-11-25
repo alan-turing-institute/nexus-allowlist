ca487510797a7adc7ad4d0c653c0bfbec7c77917
Julien Baudon <julien.baudon@arkhn.com>
2024-11-25 15:47:36 +0100

    fmt

--- nexus_allowlist/settings.py
+++ nexus_allowlist/settings.py
@@ -5,6 +5,5 @@ CRAN_REMOTE_URL = os.getenv("CRAN_REMOTE_URL", "https://cran.r-project.org/")
 APT_REMOTE_URL = os.getenv("APT_REMOTE_URL", "http://deb.debian.org/debian")
 APT_DISTRO = os.getenv("APT_DISTRO", "bookworm")
 ALLOWED_ARCHIVES = os.getenv(
-    "APT_ALLOWED_ARCHIVES",
-    "main,contrib,non-free-firmware,non-free"
+    "APT_ALLOWED_ARCHIVES", "main,contrib,non-free-firmware,non-free"
 ).split(",")
